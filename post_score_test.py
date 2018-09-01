ec548017dd958803c4ed3cea86f416702e352dc4
Auste Mastaviciute <auste.m@gmail.com>
2018-09-01 16:02:15 -0700

    last changes to the API

--- post_score_test.py
+++ post_score_test.py
@@ -2,7 +2,7 @@ import requests
 
 base_url = 'http://127.0.0.1:5000/'
 
-request1 = requests.post(base_url + 'submit_result', json={'winner': 'jwong', 'loser':'brett', 'score':'23-3',
+request1 = requests.post(base_url + 'submit_result', json={'winner': 'jwong', 'loser':'brett', 'score':'18-3',
                                                             'match_date': '2018-08-30', 'who_entered': 'damien',
                                                             'who_challenged': 'brett'})
 print(request1.text)
