ed468a0a7fe0fa5793882f5f0cb969b52a3c74ae
kiwidamien <djmartin@ucdavis.edu>
2018-09-01 11:11:38 -0700

    added database support

--- start_bot.py
+++ start_bot.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import psycopg2
+import urllib.parse as urlparse
+import os
+
 
 app = Flask('test app')
 
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
 @app.route('/')
 def index():
-  return "Hello from Flask"
+    con = psycopg2.connect(
+        dname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    cursor = con.cursor()
+    con.close()
+    return "Hello from Flask"
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
